98e81449b0e51b93c5c50090c05681166a8dc17f
Matt Morgan <mmorgan@miradorfin.com>
2018-12-07 23:06:28 -0800

    day07 part 2, needs cleanup

--- day07/day07.py
+++ day07/day07.py
@@ -1,39 +1,60 @@
 import re
 
 
-def day07(inputs):
+def day07(inputs, workers=2):
     dep_pairs = list()
     for _input in inputs:
         dep_pairs.append(re.findall(r'(?<=\s)[A-Z]', _input))
-    print(dep_pairs)
     starting_letter = 'A'
     ordered_letters = list()
     starting_letter = find_first(dep_pairs, starting_letter)
     ordered_letters.append(starting_letter)
-    print(find_next(dep_pairs, find_deps(starting_letter, dep_pairs)))
     unresolved = sorted(set([x[0] for x in dep_pairs] + [x[1] for x in dep_pairs]))
-    dep_map = dict()
-    for i in unresolved:
-        dep_map[i] = dict()
-        dep_map[i]['p'] = sorted([k[1] for k in dep_pairs if k[0] == i])
-        dep_map[i]['c'] = sorted([k[0] for k in dep_pairs if k[1] == i])
+    dep_map = build_dep_map(dep_pairs)
 
     for letter in dep_map[starting_letter]['p']:
         ordered_letters = dig(letter, dep_map, ordered_letters)
 
     unresolved.remove(starting_letter)
-    print(dep_map['E'])
     resolved = [starting_letter]
     dep_map = remove_all_parents(starting_letter, dep_map)
     while unresolved:
-        print(unresolved)
         for r in unresolved:
             if not dep_map[r]['c']:
                 unresolved.remove(r)
                 resolved.append(r)
                 dep_map = remove_all_parents(r, dep_map)
                 break
-    return ''.join(resolved), None
+
+    dep_map = build_dep_map(dep_pairs)
+    unresolved = sorted(set([x[0] for x in dep_pairs] + [x[1] for x in dep_pairs]))
+    resolved2 = []
+    inprogress = dict()
+    counter = 0
+    while unresolved:
+        for r in unresolved:
+            if not dep_map[r]['c'] and r not in inprogress:
+                if len(inprogress) < workers:
+                    inprogress[r] = ord(r) - 4
+        for t in list(sorted(inprogress.keys(), reverse=True)):
+            inprogress[t] += -1
+            if inprogress[t] == 0:
+                inprogress.pop(t)
+                unresolved.remove(t)
+                resolved2.append(t)
+                dep_map = remove_all_parents(t, dep_map)
+        counter += 1
+
+    return ''.join(resolved), counter
+
+
+def build_dep_map(dep_pairs):
+    dep_map = dict()
+    for i in sorted(set([x[0] for x in dep_pairs] + [x[1] for x in dep_pairs])):
+        dep_map[i] = dict()
+        dep_map[i]['p'] = sorted([k[1] for k in dep_pairs if k[0] == i])
+        dep_map[i]['c'] = sorted([k[0] for k in dep_pairs if k[1] == i])
+    return dep_map
 
 
 def remove_all_parents(letter, dep_map):
@@ -42,6 +63,7 @@ def remove_all_parents(letter, dep_map):
             dep_map[k]['c'].remove(letter)
     return dep_map
 
+
 def dig(letter, dep_map, resolved, q=[]):
     if not dep_map[letter]['p']:
         resolved.append(letter)
@@ -49,7 +71,6 @@ def dig(letter, dep_map, resolved, q=[]):
 
     for kid in dep_map[letter]['c']:
         if kid not in resolved:
-            print(resolved)
             resolved = dig(kid, dep_map, resolved)
     return resolved
 
--- day07/test_day07.py
+++ day07/test_day07.py
@@ -8,10 +8,10 @@ class TestDay07(TestCase):
         test_array = load_input('day07/example')
         answer_1, answer_2 = day07(test_array)
         self.assertEqual('CABDFE', answer_1)
-        self.assertEqual(None, answer_2)
+        self.assertEqual(258, answer_2)
 
     def test_day07_input(self):
         test_array = load_input('day07/input')
-        answer_1, answer_2 = day07(test_array)
+        answer_1, answer_2 = day07(test_array, workers=5)
         self.assertEqual('MNQKRSFWGXPZJCOTVYEBLAHIUD', answer_1)
-        self.assertEqual(None, answer_2)
+        self.assertEqual(948, answer_2)
